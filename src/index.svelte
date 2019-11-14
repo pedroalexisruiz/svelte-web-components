f03298ab939125effdf5fbf44e18000a7c1633c2
Pedro Ruiz <alexisruiz.selgom@gmail.com>
2019-11-13 22:54:58 -0500

    Finalizado componente en svelte

---
+++ src/index.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let name = "";
+
+  function notifiesNameChange() {
+    const event = new CustomEvent("name-changed", {
+      detail: name,
+      bubbles: true,
+      cancelable: true,
+      composed: true // makes the event jump shadow DOM boundary
+    });
+
+    this.dispatchEvent(event);
+  }
+</script>
+
+<svelte:options tag="my-component" />
+<h1>Hola mi nombre es {name}</h1>
+<input type="text" bind:value={name} on:keyup={notifiesNameChange} />
